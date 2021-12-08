6430d9fed7f41fef5a0892a2df331ec06202c846
Supanat Sripo <supanat90@gmail.com>
2021-12-08 09:22:24 +0700

    update

---
+++ routes/index.js
@@ -0,0 +1,203 @@
+var express = require("express");
+var router = express.Router();
+var User = require("../models/user");
+
+//index
+router.get("/", function (req, res, next) {
+  return res.render("login.ejs");
+});
+
+//register
+router.get("/register", function (req, res, next) {
+  return res.render("register.ejs");
+});
+router.post("/register", function (req, res, next) {
+  console.log(req.body);
+  var personInfo = req.body;
+
+  if (
+    !personInfo.email ||
+    !personInfo.username ||
+    !personInfo.password ||
+    !personInfo.passwordConf
+  ) {
+    res.send();
+  } else {
+    if (personInfo.password == personInfo.passwordConf) {
+      User.findOne({ email: personInfo.email }, function (err, data) {
+        if (!data) {
+          var c;
+          User.findOne({}, function (err, data) {
+            if (data) {
+              console.log("if");
+              c = data.unique_id + 1;
+            } else {
+              c = 1;
+            }
+
+            var newPerson = new User({
+              unique_id: c,
+              email: personInfo.email,
+              username: personInfo.username,
+              password: personInfo.password,
+              passwordConf: personInfo.passwordConf,
+            });
+
+            newPerson.save(function (err, Person) {
+              if (err) console.log(err);
+              else console.log("Success");
+            });
+          })
+            .sort({ _id: -1 })
+            .limit(1);
+          res.send({ Success: "You are regestered,You can login now." });
+        } else {
+          res.send({ Success: "Email is already used." });
+        }
+      });
+    } else {
+      res.send({ Success: "password is not matched" });
+    }
+  }
+});
+
+//login
+router.get("/login", function (req, res, next) {
+  return res.render("login.ejs");
+});
+
+router.post("/login", function (req, res, next) {
+  //console.log(req.body);
+  User.findOne({ email: req.body.email }, function (err, data) {
+    if (data) {
+      if (data.password == req.body.password) {
+        //console.log("Done Login");
+        req.session.userId = data.unique_id;
+        //console.log(req.session.userId);
+        res.send({ Success: "Success!" });
+      } else {
+        res.send({ Success: "Wrong password!" });
+      }
+    } else {
+      res.send({ Success: "This Email Is not regestered!" });
+    }
+  });
+});
+
+//profile
+router.get("/profile", function (req, res, next) {
+  console.log("profile");
+  User.findOne({ unique_id: req.session.userId }, function (err, data) {
+    console.log("data");
+    console.log(data);
+    if (!data) {
+      res.redirect("/");
+    } else {
+      //console.log("found");
+      return res.render("data.ejs", { name: data.username, email: data.email });
+    }
+  });
+});
+
+// homepage
+router.get("/homepage", function (req, res, next) {
+  console.log("homepage");
+  User.findOne({ unique_id: req.session.userId }, function (err, data) {
+    console.log("data");
+    console.log(data);
+    if (!data) {
+      res.redirect("/");
+    } else {
+      //console.log("found");
+      return res.render("homepage.ejs", {
+        name: data.username,
+        email: data.email,
+      });
+    }
+  });
+});
+
+// doctor
+router.get("/doctor", function (req, res, next) {
+  console.log("doctor");
+  User.findOne({ unique_id: req.session.userId }, function (err, data) {
+    console.log("data");
+    console.log(data);
+    if (!data) {
+      res.redirect("/");
+    } else {
+      //console.log("found");
+      return res.render("doctor.ejs", {
+        name: data.username,
+        email: data.email,
+      });
+    }
+  });
+});
+
+// h7
+router.get("/h7", function (req, res, next) {
+  console.log("h7");
+  User.findOne({ unique_id: req.session.userId }, function (err, data) {
+    console.log("data");
+    console.log(data);
+    if (!data) {
+      res.redirect("/");
+    } else {
+      //console.log("found");
+      return res.render("h7.ejs", {
+        name: data.username,
+        email: data.email,
+      });
+    }
+  });
+});
+
+//logout
+router.get("/logout", function (req, res, next) {
+  console.log("logout");
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect("/");
+      }
+    });
+  }
+});
+
+//forget password
+router.get("/forgetpass", function (req, res, next) {
+  res.render("forget.ejs");
+});
+
+router.post("/forgetpass", function (req, res, next) {
+  //console.log('req.body');
+  //console.log(req.body);
+  User.findOne({ email: req.body.email }, function (err, data) {
+    console.log(data);
+    if (!data) {
+      res.send({ Success: "This Email Is not regestered!" });
+    } else {
+      // res.send({"Success":"Success!"});
+      if (req.body.password == req.body.passwordConf) {
+        data.password = req.body.password;
+        data.passwordConf = req.body.passwordConf;
+
+        data.save(function (err, Person) {
+          if (err) console.log(err);
+          else console.log("Success");
+          res.send({ Success: "Password changed!" });
+        });
+      } else {
+        res.send({
+          Success: "Password does not matched! Both Password should be same.",
+        });
+      }
+    }
+  });
+});
+
+module.exports = router;
